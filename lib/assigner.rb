54d1701db6baf514bcf1b8ea6241c47c23337472
Meagan Waller <meaganewaller@gmail.com>
2014-01-15 17:17:28 -0600

    Adds functionality to get family size and check if two people are in the same family

--- lib/assigner.rb
+++ lib/assigner.rb
@@ -8,8 +8,23 @@ class Assigner
   end
 
   def create_families(people)
+    families = group_families_by_name(people)
+    get_families_size(people, families)
+  end
+
+  def group_families_by_name(people)
     family_name = people.map { |person| person.last_name }
     family_name.uniq!
     @families = family_name.map { |name| Family.new(name) }
+  end
+
+  def get_families_size(people, families)
+    families.each do |family|
+      family.size = people.count { |person| person.last_name == family.name }
+    end
+  end
+
+  def same_family?(first_person, second_person)
+    first_person.last_name == second_person.last_name
   end
 end
--- spec/assigner_spec.rb
+++ spec/assigner_spec.rb
@@ -5,11 +5,11 @@ require './lib/secret_santa'
 
 describe Assigner do
 
-  # let(:meagan) { Person.new("Meagan", "Waller", "meagan@waller.com") }
-  # let(:lauren) { Person.new("Lauren", "Waller", "lauren@waller.com") }
-  # let(:riley) { Person.new("Riley", "Radoll", "riley@radoll.com") }
-  # let(:sarah) { Person.new("Sarah", "Smith", "sarah@smith.com")}
-  # let(:charles) { Person.new("Charles", "Miner", "charles@miner.com")}
+   let(:meagan) { Person.new("Meagan", "Waller", "meagan@waller.com") }
+   let(:lauren) { Person.new("Lauren", "Waller", "lauren@waller.com") }
+   let(:riley) { Person.new("Riley", "Radoll", "riley@radoll.com") }
+   let(:sarah) { Person.new("Sarah", "Smith", "sarah@smith.com")}
+   let(:charles) { Person.new("Charles", "Miner", "charles@miner.com")}
   
   let(:people) { SecretSanta.new.create_people_from_file("./spec/test_file.txt") }
   let(:assigner) { Assigner.new(people) }
@@ -24,5 +24,21 @@ describe Assigner do
     families[4].name.should == "Halpert"
     families[5].name.should == "Schrute"
   end
+
+  it "has family size" do
+    families = assigner.families
+    families[0].size.should == 2
+    families[1].size.should == 1
+  end
+
+  it "returns true if people are in the same family" do
+    assigner.same_family?(meagan, lauren).should be_true
+  end
+
+  it "returns false if people aren't in the same family" do
+    assigner.same_family?(meagan, riley).should be_false
+  end
+
+  
   
 end
