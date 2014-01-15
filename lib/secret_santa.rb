6f3978f02db42a825374de8d01dbd6ee414950a5
Meagan Waller <meaganewaller@gmail.com>
2014-01-15 16:46:57 -0600

    Adds spec for the secretsanta for creating people from file

--- lib/secret_santa.rb
+++ lib/secret_santa.rb
@@ -1,23 +1,13 @@
+require_relative 'person'
+
 class SecretSanta
-  attr_accessor :first_name, :last_name, :email
-  def initialize(file)
-    file = File.read(file)
-    parse_file(file)
-  end
-
-  def parse_file(file)
-    file.each_line do |line|
-      line.match(/(\S+)\s+(\S+)\s+<(.*)>/)
-      first_name, last_name, email = $1, $2, $3
-      get_full_name(first_name, last_name)
+  def create_people_from_file(file)
+    people = []
+    IO.foreach(file) do |line|
+      details = line.split
+      first_name, last_name, email = details[0], details[1], details[2].delete('<>')
+      people << Person.new(first_name, last_name, email)
     end
-  end
-
-  def get_full_name(first, last)
-    first + " " + last
+    people
   end
 end
-
-SecretSanta.new('./lib/secret_santa_file.txt')
-
-
