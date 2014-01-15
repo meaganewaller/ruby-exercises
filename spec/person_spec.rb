c2c3bbb2d5142727bec67d49c16fb74982690dc5
Meagan Waller <meaganewaller@gmail.com>
2014-01-15 16:32:38 -0600

    Changes tests to match new arguments in person

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,22 +2,15 @@ require 'spec_helper'
 require './lib/person'
 
 describe Person do
-  meagan_details = { 
-    "first_name" => "Meagan", 
-    "last_name"  => "Waller", 
-    "email"      => "meagan@something.com" 
-  }
-
   it "initializes with persons details" do
-    person = Person.new(meagan_details)
+    person = Person.new("Meagan", "Waller", "meagan@something.com")
     person.first_name.should == "Meagan"
     person.last_name.should == "Waller"
     person.email.should == "meagan@something.com"
   end
 
   it "prints name nicely" do
-    meagan = Person.new(meagan_details)
-    bob = Person.new(bob_details)
-    meagan.to_s.should == "Meagan Waller"
+    person = Person.new("Meagan", "Waller", "meagan@waller.com")
+    person.to_s.should == "Meagan Waller"
   end
 end
