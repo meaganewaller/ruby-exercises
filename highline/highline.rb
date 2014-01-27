144dfafa1eb9032322ea41308cba4624a21ab76b
Meagan Waller <meaganewaller@gmail.com>
2014-01-27 16:51:11 -0600

    Comment out unused code for now

--- highline/highline.rb
+++ highline/highline.rb
@@ -2,57 +2,57 @@
 
 module HighLine
   class CLI
-    def ask(question, options=nil)
-      begin
-        # Starts off as valid
-        valid = true
+    # def ask(question, options=nil)
+    #   begin
+    #     # Starts off as valid
+    #     valid = true
 
-        # Gets default, and gets default string if default exists, otherwise ""
-        default = option(options, :default)
-        default_str = get_default_string(default)
+    #     # Gets default, and gets default string if default exists, otherwise ""
+    #     default = option(options, :default)
+    #     default_str = get_default_string(default)
 
-        # When asking for integers this takes a number in either binary, octal, decimal or hexadecimal
-        base = option(options, :base)
+    #     # When asking for integers this takes a number in either binary, octal, decimal or hexadecimal
+    #     base = option(options, :base)
 
-        # Print the question to the user, dsiplaying the default string
-        print question, "#{default_str} "
+    #     # Print the question to the user, dsiplaying the default string
+    #     print question, "#{default_str} "
 
-        # Get the input, if no input, resort to default if it exists
-        input = gets.chomp
-        if default && input == ""
-          input = default
-        end
+    #     # Get the input, if no input, resort to default if it exists
+    #     input = gets.chomp
+    #     if default && input == ""
+    #       input = default
+    #     end
         
-        # Convert the input to the correct type
-        input = case type
-                when :string: input
-                when :integer: convert(input, base) rescue valid = false
-                when :float: Float(input) rescue valid = false
-                when :bool:
-                  valid = input =~ /^(y|n|yes|no)$/
-                  input[0] == ?y
-                end
+    #     # Convert the input to the correct type
+    #     input = case type
+    #             when :string: input
+    #             when :integer: convert(input, base) rescue valid = false
+    #             when :float: Float(input) rescue valid = false
+    #             when :bool:
+    #               valid = input =~ /^(y|n|yes|no)$/
+    #               input[0] == ?y
+    #             end
 
-        # Validating the input
-        valid &&= validate(options, :validate) { |value| input =~ value }
-        valid &&= validate(options, :within) { |value| input > value }
-        valid &&= validate(options, :above) { |value| input > value }
-        valid &&= validate(options, :below) { |value| input < value }
+    #     # Validating the input
+    #     valid &&= validate(options, :validate) { |value| input =~ value }
+    #     valid &&= validate(options, :within) { |value| input > value }
+    #     valid &&= validate(options, :above) { |value| input > value }
+    #     valid &&= validate(options, :below) { |value| input < value }
 
-        puts "Not a valid value" unless valid
-        end until valid
+    #     puts "Not a valid value" unless valid
+    #     end until valid
 
-        return input
-      end
-    end
+    #     return input
+    #   end
+    # end
 
-    def get_default_string(default)
-      if default
-        " |#{default}|"
-      else
-        ""
-      end
-    end
+    # def get_default_string(default)
+    #   if default
+    #     " |#{default}|"
+    #   else
+    #     ""
+    #   end
+    # end
 
     def yes_no?(question, options = {})
       print "#{question} [y/n] "
